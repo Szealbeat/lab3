cc3db3c651769d4aa64481245427ca5aefcd6b3f
Konstantin Kovalenko <konstantinkovalenko847@gmail.com>
2023-10-08 19:30:01 +0700

    First project

---
+++ First/first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main(void) {
+	setlocale(LC_ALL, "rus");
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+
+	cout << "Привет!" << endl;
+	//system("pause");
+
+	return 0;
+}
